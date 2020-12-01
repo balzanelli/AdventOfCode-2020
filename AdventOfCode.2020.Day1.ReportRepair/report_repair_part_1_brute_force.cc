49b70c8001e3ff5a25c687211373537a181f0792
balzanelli <me@balzanel.li>
2020-12-01 20:56:19 +0000

    fix: day1: part1 brute force off by one

--- AdventOfCode.2020.Day1.ReportRepair/report_repair_part_1_brute_force.cc
+++ AdventOfCode.2020.Day1.ReportRepair/report_repair_part_1_brute_force.cc
@@ -3,8 +3,8 @@
 namespace AdventOfCode2020::Day1::ReportRepair::Part1::BruteForce
 {
   int Solve(const std::vector<int>& data_set, const int sum) {
-    for (std::size_t i = 0; i < data_set.size(); i++) {
-      for (std::size_t j = i + 1; j < data_set.size() - 1; j++) {
+    for (std::size_t i = 0; i < data_set.size() - 1; i++) {
+      for (std::size_t j = i + 1; j < data_set.size(); j++) {
         if (data_set[i] + data_set[j] == sum) {
           return data_set[i] * data_set[j];
         }
